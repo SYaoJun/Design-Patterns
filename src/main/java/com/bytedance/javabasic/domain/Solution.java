8ed018109646986066a9878f8f079ed18739cef1
SYaoJun <940334249@qq.com>
2020-12-30 15:06:10 +0800

    add a dream company index

---
+++ src/main/java/com/bytedance/javabasic/domain/Solution.java
@@ -0,0 +1,26 @@
+package com.bytedance.javabasic.domain;
+
+import org.springframework.expression.spel.ast.Literal;
+
+import java.util.Stack;
+import java.util.stream.Stream;
+
+/**
+ * @author yaojun
+ * @create 2020-12-18 13:53
+ */
+
+
+class Solution {
+    public void rotate(int[][] matrix) {
+    }
+}
+
+
+
+class Test{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
